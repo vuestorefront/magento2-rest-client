cff22dd308aecf53158140ee5ee8a32bff472711
David Watson <davidlwatsonjr@users.noreply.github.com>
2019-09-10 11:04:00 -0400

    Add transactions endpoints

--- index.js
+++ index.js
@@ -15,42 +15,42 @@ var customers = require('./lib/customers');
 var directory = require('./lib/directory');
 var cart = require('./lib/cart');
 var orders = require('./lib/orders');
+var transactions = require('./lib/transactions');
 var reviews = require('./lib/reviews');
 
 const MAGENTO_API_VERSION = 'V1';
 
-module.exports.Magento2Client = function (options) {
-    var instance = {
-        addMethods (key, module) {
-            var client = RestClient(options);
-            if (module) {
-                if (this[key])
-                    this[key] = Object.assign(this[key], module(client))
-                else 
-                    this[key] = module(client)
-            }
-        }
-    };
+module.exports.Magento2Client = function(options) {
+  var instance = {
+    addMethods(key, module) {
+      var client = RestClient(options);
+      if (module) {
+        if (this[key]) this[key] = Object.assign(this[key], module(client));
+        else this[key] = module(client);
+      }
+    }
+  };
 
-    options.version = MAGENTO_API_VERSION;
-    
-    var client = RestClient(options);
+  options.version = MAGENTO_API_VERSION;
 
-    instance.attributes = attributes(client);
-    instance.categories = categories(client);
-    instance.products = products(client);
-    instance.productMedia = productMedia(client);
-    instance.categoryProducts = categoryProducts(client);
-    instance.configurableChildren = configurableChildren(client);
-    instance.configurableOptions = configurableOptions(client);
-    instance.stockItems = stockItems(client);
-    instance.taxRates = taxRates(client);
-    instance.taxRules = taxRules(client);
-    instance.customers = customers(client);
-    instance.cart = cart(client);
-    instance.orders = orders(client);
-    instance.directory = directory(client);
-    instance.reviews = reviews(client);
+  var client = RestClient(options);
 
-    return instance;
-}
+  instance.attributes = attributes(client);
+  instance.categories = categories(client);
+  instance.products = products(client);
+  instance.productMedia = productMedia(client);
+  instance.categoryProducts = categoryProducts(client);
+  instance.configurableChildren = configurableChildren(client);
+  instance.configurableOptions = configurableOptions(client);
+  instance.stockItems = stockItems(client);
+  instance.taxRates = taxRates(client);
+  instance.taxRules = taxRules(client);
+  instance.customers = customers(client);
+  instance.cart = cart(client);
+  instance.orders = orders(client);
+  instance.transactions = transactions(client);
+  instance.directory = directory(client);
+  instance.reviews = reviews(client);
+
+  return instance;
+};
